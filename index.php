b7f865efb8a41f0ae04a78cc45ab5ca508337451
Flighteur <pit.brun@hotmail.fr>
2014-12-27 14:03:45 +0100

    minimal home page setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<header>
+		<?php include "header.php" ?> 
+	</header>
+	<body>
+		<?php include "body.php" ?>
+	</body>
+	<footer>
+		<?php include "footer.php" ?>
+	</footer>
+</html>
