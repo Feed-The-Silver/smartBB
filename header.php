02a8cf74e5071bf2f069a75ed22073ea7bc7e49b
Flighteur <pit.brun@hotmail.fr>
2015-01-31 20:54:01 +0100

    syncing bugs resolved
    synicing bug resolved : form duplicata

--- header.php
+++ header.php
@@ -2,15 +2,7 @@
 <h2 class="subtitle">Powerful, Intuitive, Beautiful</h2>
 <form action="connexion.php" method="post" accept-charset="utf-8">
 	<input type="text" name="pseudo" placeholder="Pseudo">
-<<<<<<< HEAD
-	<input type="text" name="mdp" placeholder="Mot de passe">
-	<input type="submit" name="Submit" value="Connexion">
-	<script src="jquery.js"></script>
-</form>
-=======
-        <input type="password" name="mdp" placeholder="Mot de passe">
-        <input type="submit" name="Submit" value="Connexion"><br/>
-        
+    <input type="password" name="mdp" placeholder="Mot de passe">
+    <input type="submit" name="Submit" value="Connexion"><br/>       
 </form>
 <p>Not signed up yet?<a href="inscription.php">Register</a></p>
->>>>>>> origin/master
--- table_generator.php
+++ table_generator.php
@@ -1,6 +1,6 @@
 <form action="table_generator.php" method="post" accept-chaset="utf-8">
-	<label><input type="number" id="column" name="columnNumber" value="3" min="1" max="5"> Nombre de Colonne(s)</label>
-	<!--<input type="submit" name="valider" value="Valider">-->
+	<label>Nombre de Colonne(s)<input type="number" id="column" name="columnNumber" value="3" min="1" max="5"></label>
+	<input type="submit" name="valider" value="Valider">
 </form>
 <?php
 
